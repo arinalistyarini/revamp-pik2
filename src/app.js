f8914d883c1281d0244be0735a17e47b68baa4de
Arina Listyarini D <arina.listyarini@mekari.com>
2022-03-17 12:56:15 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/* src/app.js */
+
+// Styles
+import 'styles/_app.scss';
+
+// Libraries
+import 'bootstrap';
+import 'slick-carousel';
+
+// Scripts
+$(() => {
+  require('scripts/scroll');
+  require('scripts/carousel');
+});
---
+++ src/assets/scripts/carousel.js
@@ -0,0 +1,9 @@
+/* src/assets/scripts/carousel.js */
+
+$('.carousel').slick({
+  arrows: false,
+  autoplay: true,
+  fade: true,
+  infinite: true,
+  speed: 250,
+});
