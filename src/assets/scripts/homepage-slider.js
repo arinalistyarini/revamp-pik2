f1802bf624112ee893bf21651c4cd93a64520acf
Arina Listyarini D <arina.listyarini@mekari.com>
2022-03-20 21:14:12 +0700

    feat: slicing product listing fix: new in child menu chore: build

--- src/assets/scripts/homepage-slider.js
+++ src/assets/scripts/homepage-slider.js
@@ -7,7 +7,7 @@ if (isBannerSliderAvailable) {
     $(this).slick({
       autoplay: true,
       infinite: true,
-      speed: 250,
+      speed: 500,
       prevArrow: '<a class="arrow-slider arrow-slide-left"><i class="icon-pik ip-arrow-left"></i></a>',
       nextArrow: '<a class="arrow-slider arrow-slide-right"><i class="icon-pik ip-arrow-right"></i></a>',
     });
