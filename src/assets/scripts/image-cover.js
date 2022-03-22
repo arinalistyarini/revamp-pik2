3f0dd247ab08a3bb7db7c9cbfbddbc3028fd981a
Arina Listyarini D <arina.listyarini@mekari.com>
2022-03-22 09:06:59 +0700

    feat: slicing investment opportunity chore: build

--- src/assets/scripts/image-cover.js
+++ src/assets/scripts/image-cover.js
@@ -9,21 +9,23 @@ export default function imageCover() {
     }
 
     const containerRatio = container.innerWidth() / container.innerHeight();
-    wrapper.find('img').each(function getImage() {
-      const image = $(this);
-
-      // image finish loading
-      // https://stackoverflow.com/a/60971078
-      const imgElement = new Image();
-      imgElement.src = image.attr('src');
-
-      imgElement.decode().then(() => {
-        const imageRatio = imgElement.width / imgElement.height;
-        const isFillHeight = imageRatio > containerRatio;
-        wrapper.removeClass('fill-height');
-        wrapper.removeClass('fill-width');
-        wrapper.addClass(isFillHeight ? 'fill-height' : 'fill-width');
+    if (container.is('[class*=ratio-]') || container.is('[class^=ratio-]') || container.hasClass('full-height')) {
+      wrapper.find('img').each(function getImage() {
+        const image = $(this);
+  
+        // image finish loading
+        // https://stackoverflow.com/a/60971078
+        const imgElement = new Image();
+        imgElement.src = image.attr('src');
+  
+        imgElement.decode().then(() => {
+          const imageRatio = imgElement.width / imgElement.height;
+          const isFillHeight = imageRatio > containerRatio;
+          wrapper.removeClass('fill-height');
+          wrapper.removeClass('fill-width');
+          wrapper.addClass(isFillHeight ? 'fill-height' : 'fill-width');
+        });
       });
-    });
+    }
   });
 }
