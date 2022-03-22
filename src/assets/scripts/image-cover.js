3f7011c101525fc429bb46bf23c9d64d8db8cf48
Arina Listyarini D <arina.listyarini@mekari.com>
2022-03-22 09:08:27 +0700

    fix: error chore: build

--- src/assets/scripts/image-cover.js
+++ src/assets/scripts/image-cover.js
@@ -12,12 +12,12 @@ export default function imageCover() {
     if (container.is('[class*=ratio-]') || container.is('[class^=ratio-]') || container.hasClass('full-height')) {
       wrapper.find('img').each(function getImage() {
         const image = $(this);
-  
+
         // image finish loading
         // https://stackoverflow.com/a/60971078
         const imgElement = new Image();
         imgElement.src = image.attr('src');
-  
+
         imgElement.decode().then(() => {
           const imageRatio = imgElement.width / imgElement.height;
           const isFillHeight = imageRatio > containerRatio;
